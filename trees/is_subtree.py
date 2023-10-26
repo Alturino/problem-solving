bfd7c27c05ddd62e4ef5f5372d1d8b9f5d9e6906
Ricky <alturino001@gmail.com>
2023-10-26 21:17:50 +0700

    add more problems

---
+++ trees/is_subtree.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val:int = 0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, s:Optional[TreeNode], t:Optional[TreeNode]) -> bool:
+        if not t:
+            return True
+        if not s:
+            return False
+  
+        if self.sameTree(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+    def sameTree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+        return False
+
+s = Solution()
+root = TreeNode(3, left=TreeNode(4, left=TreeNode(1), right=TreeNode(2, left=TreeNode(0))), right=TreeNode(5))
+subRoot = TreeNode(4, left=TreeNode(1), right=TreeNode(2))
+print(s.isSubtree(root, subRoot))
---
+++ trees/same_tree.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val:int = 0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+s = Solution()
+# p = TreeNode(1 ,left=TreeNode(2))
+# q = TreeNode(1 ,right=TreeNode(2))
+p = TreeNode(1,right=TreeNode(3), left=TreeNode(2))
+q = TreeNode(1,right=TreeNode(3), left=TreeNode(2))
+print(s.isSameTree(p, q))
