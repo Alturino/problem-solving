8578d985ed1adcf357bbeedd29767be951f3526b
Ricky Alturino <alturino001@gmail.com>
2023-11-08 22:42:54 +0700

    fix bug in graph/pacific_atlantic_water_flow.py

--- graph/pacific_atlantic_water_flow.py
+++ graph/pacific_atlantic_water_flow.py
@@ -17,21 +17,20 @@ class Solution:
             ):
                 return
 
-            if heights[r][c] >= prevHeight:
-                visited.add((r, c))
+            visited.add((r, c))
 
             dfs(r + 1, c, visited, heights[r][c])
             dfs(r - 1, c, visited, heights[r][c])
             dfs(r, c + 1, visited, heights[r][c])
             dfs(r, c - 1, visited, heights[r][c])
 
-        for r in range(rows):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, cols - 1, atl, heights[r][cols - 1])
-
         for c in range(cols):
             dfs(0, c, pac, heights[0][c])
             dfs(rows - 1, c, atl, heights[rows - 1][c])
+
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
 
         res = []
         for r in range(rows):
