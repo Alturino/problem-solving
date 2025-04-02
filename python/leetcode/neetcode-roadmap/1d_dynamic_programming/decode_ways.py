14d133c1d0184071feaa071d38a2ea083c4dc13e
Ricky Alturino <alturino001@gmail.com>
2025-04-02 11:45:06 +0700

    feat(python): add decode_ways in 1d_dynamic_programming

--- python/leetcode/neetcode-roadmap/1d_dynamic_programming/decode_ways.py
+++ python/leetcode/neetcode-roadmap/1d_dynamic_programming/decode_ways.py
@@ -1,12 +1,65 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        m = {i + 1: chr(i + ord("a")) for i in range(26)}
+        prev = prev_2 = 0
+        res = 1
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                prev = 0
+            else:
+                prev = res
 
-        def decode(i: int, currS: str):
-            if i >= 27:
-                return 0
-            if s == "0":
-                return 0
+            if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] < "7"):
+                prev += prev_2
 
-            decode(i + 1, s[i + 1])
-            decode(i + 2, s[i : i + 1])
+            prev_2 = res
+            res = prev
+            prev = 0
+
+        return res
+
+    # def numDecodings(self, s: str) -> int:
+    #     dp = {len(s): 1}
+    #     for i in range(len(s) - 1, -1, -1):
+    #         if s[i] == "0":
+    #             dp[i] = 0
+    #         else:
+    #             dp[i] = dp[i + 1]
+    #
+    #         if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] < "7"):
+    #             dp[i] += dp[i + 2]
+    #     return dp[0]
+
+    # def numDecodings(self, s: str) -> int:
+    #     cache = {}
+    #
+    #     def recursive(step: int = 0) -> int:
+    #         if step == len(s):
+    #             return 1
+    #         if s[step] == "0":
+    #             return 0
+    #         if step in cache:
+    #             return cache[step]
+    #
+    #         res = recursive(step + 1)
+    #         if step < len(s) - 1:
+    #             if s[step] == "1" or (s[step] == "2" and s[step + 1] < "7"):
+    #                 res += recursive(step + 2)
+    #         cache[step] = res
+    #         return res
+    #
+    #     return recursive()
+
+    # def numDecodings(self, s: str) -> int:
+    #     def recursive(step: int = 0) -> int:
+    #         if step == len(s):
+    #             return 1
+    #         if s[step] == "0":
+    #             return 0
+    #
+    #         res = recursive(step + 1)
+    #         if step < len(s) - 1:
+    #             if s[step] == "1" or (s[step] == "2" and s[step + 1] < "7"):
+    #                 res += recursive(step + 2)
+    #         return res
+    #
+    #     return recursive()
