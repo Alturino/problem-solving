8ae808a08468e467d01a36441bc519bbf27325d8
Ricky Alturino <alturino001@gmail.com>
2025-04-03 18:44:09 +0700

    feat(python): add more solution to coin_change

--- python/leetcode/neetcode-roadmap/1d_dynamic_programming/coin_change.py
+++ python/leetcode/neetcode-roadmap/1d_dynamic_programming/coin_change.py
@@ -2,6 +2,19 @@ from typing import List
 
 
 class Solution:
+    # Iterative dp bottom-up
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+    # # Recursive dp memoization bottom-up
     # def coinChange(self, coins: List[int], amount: int):
     #     minCount = float("inf")
     #     dp = [0] * (amount + 1)
@@ -22,16 +35,40 @@ class Solution:
     #     backtracking(0, 0, 0)
     #     return minCount
 
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
+    # # Recursive top-down memoization
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+    #     cache = {}
+    #
+    #     def rec(curr_amount: int = amount) -> int:
+    #         if curr_amount == 0:
+    #             return 0
+    #         if curr_amount in cache:
+    #             return cache[curr_amount]
+    #
+    #         res = 1 << 64
+    #         for coin in coins:
+    #             if curr_amount - coin >= 0:
+    #                 res = min(res, 1 + rec(curr_amount - coin))
+    #         cache[curr_amount] = res
+    #         return res
+    #
+    #     res = rec()
+    #     return res if res != 1 << 64 else -1
 
-        for a in range(1, amount + 1):
-            for coin in coins:
-                if a - coin >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - coin])
-
-        return dp[amount] if dp[amount] != amount + 1 else -1
+    # # Recursive top-down
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+    #     def rec(curr_amount: int = amount) -> int:
+    #         if curr_amount == 0:
+    #             return 0
+    #
+    #         res = 1 << 64
+    #         for coin in coins:
+    #             if curr_amount - coin >= 0:
+    #                 res = min(res, 1 + rec(curr_amount - coin))
+    #         return res
+    #
+    #     res = rec()
+    #     return res if res != 1 << 64 else -1
 
 
 s = Solution()
